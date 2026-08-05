b122878e5c8d5285adaca9e676f4f1ce3eb35685
HIMANSHI <hi3117sh@gmail.com>
2026-08-05 16:51:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,57 @@
 import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
 import cors from "cors";
-import bookRoutes from "./routes/books.js";
+import dotenv from "dotenv";
+
+import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import favoriteRoutes from "./routes/favoriteRoutes.js";
+import bookRoutes from "./routes/books.js";
 
 dotenv.config();
-console.log("JWT SECRET:", process.env.JWT_SECRET);
 
 const app = express();
 
-app.use(cors());
+
+// Database
+connectDB();
+
+
+// CORS Configuration
+app.use(
+  cors({
+    origin: [
+      "https://bookscout-starter-fullstack.vercel.app",
+      "https://bookscout-starter-fullstack-fh5el5wkr-hima3117s-projects.vercel.app"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+
+
+// Middleware
 app.use(express.json());
 
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/favorites", favoriteRoutes);
 app.use("/api/books", bookRoutes);
 
 
+// Test API
 app.get("/", (req, res) => {
-  res.send("BookScout Backend Running 🚀");
+  res.json({
+    message: "BookScout Backend Running 🚀"
+  });
 });
 
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("MongoDB Connected");
+// Server
+const PORT = process.env.PORT || 5000;
 
-  app.listen(process.env.PORT || 5000, () => {
-    console.log("Server running on port 5000");
-  });
-})
-.catch((err) => {
-  console.log("MongoDB Error:", err.message);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
