7c7b098b31c0ec7fccfcd4fbb883cc566dc64e2f
HIMANSHI <hi3117sh@gmail.com>
2026-08-14 15:06:18 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: " https://bookscout-starter-fullstack.onrender.com",
+  baseURL: "https://bookscout-starter-fullstack.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
